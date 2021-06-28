fbc8afbedc34c2ab49bd8a9b6b1c516d189af6d2
Chrishanth <chrishanth@mirrar.in>
2021-06-28 12:12:03 +0530

    TDD - step 3 - Add 2 integers pass

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -5,6 +5,10 @@ public class StringCalculator {
     public int add(String number) {
         if (isEmpty(number))
             return 0;
+        if (number.contains(",")) {
+            String[] numbers = number.split(",");
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        }
         return Integer.parseInt(number);
     }
 
